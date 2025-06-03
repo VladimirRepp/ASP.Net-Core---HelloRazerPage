b927c10e143cecf6c294f431860bf185393287da
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-06-03 18:22:24 +0700

    Add files via upload

---
+++ HelloRazerPages - backup/HelloRazerPages/Pages/UsersPage.cshtml.cs
@@ -0,0 +1,46 @@
+using HelloRazerPages.Models;
+using HelloRazerPages.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloRazerPages.Pages
+{
+    public class UsersPage : PageModel
+    {
+        private readonly UserService _userService;
+
+        [BindProperty]
+        public User CurrentUser { get; set; } = new User();
+
+        public List<User> Users => _userService.GetAll();
+
+        public UsersPage(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public void OnGet() { }
+
+        public JsonResult OnGetUser(int id)
+        {
+            var user = _userService.GetById(id);
+            return new JsonResult(user);
+        }
+
+        public IActionResult OnPostSave()
+        {
+            if (CurrentUser.Id == 0)
+                _userService.Add(CurrentUser);
+            else
+                _userService.Update(CurrentUser);
+
+            return RedirectToPage();
+        }
+
+        public JsonResult OnPostDelete(int id)
+        {
+            _userService.Delete(id);
+            return new JsonResult(new { success = true });
+        }
+    }
+}
---
+++ HelloRazerPages - backup/HelloRazerPages/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using HelloRazerPages.Models;
+
+namespace HelloRazerPages.Services
+{
+    public class UserService
+    {
+        private List<User> _users;
+        private int _nextId = 1;
+
+        public UserService()
+        {
+            _users = new List<User>
+            {
+                new User { Id = 1, Login = "user1", Password = "pass1" },
+                new User { Id = 2, Login = "user2", Password = "pass2" }
+            };
+        }
+
+        public List<User> GetAll() => _users;
+
+        public User GetById(int id) => _users.FirstOrDefault(u => u.Id == id);
+
+        public void Add(User user)
+        {
+            user.Id = _nextId++;
+            _users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            var existingUser = GetById(user.Id);
+            if (existingUser != null)
+            {
+                existingUser.Login = user.Login;
+                existingUser.Password = user.Password;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var user = GetById(id);
+            if (user != null)
+            {
+                _users.Remove(user);
+            }
+        }
+    }
+}
