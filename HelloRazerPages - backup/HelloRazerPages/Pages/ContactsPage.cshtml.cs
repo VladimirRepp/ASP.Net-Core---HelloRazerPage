e42961e0e918e24340a501b39d9980367bf0a984
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-06-11 14:52:35 +0700

    Add files via upload

---
+++ HelloRazerPages - backup/HelloRazerPages/Pages/ContactsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloRazerPages.Pages
+{
+    public class ContactsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HelloRazerPages - backup/HelloRazerPages/Pages/UsersPage.cshtml.cs
@@ -0,0 +1,48 @@
+using HelloRazerPages.Models;
+using HelloRazorPages.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloRazerPages.Pages
+{
+    public class UsersPage : PageModel
+    {
+        private readonly UserService _userService;
+
+        [BindProperty]
+        public User CurrentUser { get; set; } = new User();
+
+        public List<User> Users => _userService.GetAll();
+
+        public UsersPage(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public void OnGet() { }
+
+        public JsonResult OnGetUser(int id)
+        {
+            var user = _userService.GetById(id);
+            return new JsonResult(user);
+        }
+
+        public IActionResult OnPostSave()
+        {
+            bool isResult; 
+            
+            if (CurrentUser.Id == 0)
+                isResult = (CurrentUser = _userService.Add(CurrentUser)).Id != 0 ? true : false;
+            else
+                isResult = _userService.Update(CurrentUser);
+
+            return RedirectToPage();
+        }
+
+        public JsonResult OnPostDelete(int id)
+        {
+            bool isResult = _userService.Delete(id);
+            return new JsonResult(new { success = isResult });
+        }
+    }
+}
